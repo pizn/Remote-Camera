d9a199064d5f9824c2529334402963712a49f44e
pizn <pizner@gmail.com>
2014-01-26 21:26:03 +0800

    add loading

--- webapp/script/index.js
+++ webapp/script/index.js
@@ -8,7 +8,14 @@ angular.module('EasyCamera',['ngResource']).config(function($routeProvider) {
 });
 
 function cameraCtrl($scope, $resource) {
+    $scope.picture = 'thumb.jpg';
+    $scope.width = '320';
+    $scope.height = '240';
     $scope.cameraAction = function() {
+        $scope.picture = 'loading.gif';
+        $scope.width = '48';
+        $scope.height = '48';
+        $scope.class = 'loading';
         /**
          *  发起请求，拍照，返回结果
          */
@@ -16,13 +23,22 @@ function cameraCtrl($scope, $resource) {
             //塞入图片
             if(data.stat === 'ok') {
                 $scope.picture = data.id + '.jpg';
+                $scope.width = '320';
+                $scope.height = '240';
+                $scope.class = '';
             } else {
                 $scope.errorMsg = 'Sorry, I can\'t get the photo';
                 $scope.picture = 'thumb.jpg';
+                $scope.width = '320';
+                $scope.height = '240';
+                $scope.class = '';
             }
         }, function(e) {
             $scope.errorMsg = 'fail';
             $scope.picture = 'thumb.jpg';
+            $scope.width = '320';
+            $scope.height = '240';
+            $scope.class = '';
         });
 
     }
