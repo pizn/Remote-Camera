8b2b27dc388d32b127da1816320a0755e092e361
pizn <pizner@gmail.com>
2014-01-26 22:53:21 +0800

    change route and btn style when action

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var config = require(__dirname + '/config.js');
 var express = require('express');
 var app = express();
 
-// app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.static(__dirname + '/webapp'));
--- webapp/script/index.js
+++ webapp/script/index.js
@@ -11,6 +11,7 @@ function cameraCtrl($scope, $resource) {
     $scope.picture = 'thumb.jpg';
     $scope.cameraAction = function() {
         $scope.picture = 'loading.jpg';
+        $scope.doAction = true;
         /**
          *  发起请求，拍照，返回结果
          */
@@ -18,13 +19,16 @@ function cameraCtrl($scope, $resource) {
             //塞入图片
             if(data.stat === 'ok') {
                 $scope.picture = data.id + '.jpg';
+                $scope.doAction = false;
             } else {
                 $scope.errorMsg = 'Sorry, I can\'t get the photo';
                 $scope.picture = 'thumb.jpg';
+                $scope.doAction = false;
             }
         }, function(e) {
             $scope.errorMsg = 'fail';
             $scope.picture = 'thumb.jpg';
+            $scope.doAction = false;
         });
 
     }
